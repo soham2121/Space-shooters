caea5e4681848845546e012aab7808459a0b7a8c
GamingStudios21 <56083422+soham2121@users.noreply.github.com>
2021-02-24 11:13:07 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,16 +1,18 @@
-var spaceshipimg1, spaceshipimg2, spaceshipimg3, spaceship, playbutton, background1, backgroundimg;
-var countdownimg1, countdownimg2, countdownimg3, countdownimg0, countdown, starimg;
-var gamestate = "menu", mySound, countdwn = 4, stararr = [], score = 0;
+var spaceshipimg1, spaceshipimg2, spaceshipimg3, spaceship, playbutton, background1, backgroundimg, pausebutton;
+var countdownimg1, countdownimg2, countdownimg3, countdownimg0, countdown, starimg, pauseimg, playimg;
+var gamestate = "menu", mySound, countdwn = 4, stararr = [], score = 0, incspd = 0, canmove, pauseState = false, canClickPause = true;
 
 function preload(){
   spaceshipimg1 = loadImage("sprites/menu.png");
   spaceshipimg2 = loadImage("sprites/countdown.png");
-  spaceshipimg3 = loadImage("sprites/final.jpg");
+  spaceshipimg3 = loadImage("sprites/final.png");
   countdownimg0 = loadImage("sprites/0.png");
   countdownimg1 = loadImage("sprites/1.png");
   countdownimg2 = loadImage("sprites/2.png");
   countdownimg3 = loadImage("sprites/3.png");
-  starimg = loadImage("sprites/star.png")
+  starimg = loadImage("sprites/star.png");
+  pauseimg = loadImage("sprites/pause.png");
+  playimg = loadImage("sprites/play.png");
   soundFormats('mp3', 'ogg');
   mySound = loadSound('sounds/start.mp3');
 }
@@ -18,42 +20,46 @@ function preload(){
 function setup() {
   createCanvas(displayWidth, displayHeight-110);
 
-  playbutton = createSprite(738,200,100,40);
+  playbutton = createSprite(displayWidth/2,200,100,40);
   playbutton.shapeColor = "white";
 
-  spaceship = createSprite(748,displayHeight + 20,10,10);
+  spaceship = createSprite(displayWidth/2,displayHeight + 20,10,10);
   spaceship.setCollider("rectangle",0, -15, 50, 50, 0);
   spaceship.addImage(spaceshipimg1);
 
-  countdown = createSprite(displayWidth/2-30, displayHeight/2-150, 1000, 500);
+  countdown = createSprite(displayWidth/2, displayHeight/2-150, 1000, 500);
   countdown.shapeColor = "black";
   countdown.visible = false;
+
+  pausebutton = createSprite(50, 50, 20, 20);
+  pausebutton.addImage(pauseimg);
+  pausebutton.visible = false;
+
+  createStars(180);
 }
 
 function draw() {
   background(0);
   
   drawSprites();
-
-  if(frameCount === 1){
-    createStars(180);
-  }
   
   if(gamestate === "menu"){
+    incspd = 0;
     textSize(20);
     textFont("Geiorgia");
     fill("black");
-    text("Play",723,205);
+    text("Play",playbutton.x-(textWidth("Play")/2),playbutton.y+5);
     spaceship.addImage(spaceshipimg1);
     if(mousePressedOver(playbutton)){
-    gamestate="moving";
-   }
+      gamestate="moving";
+      incspd = 0.5;
+    }
   }
 
    else if(gamestate === "moving"){
     playbutton.destroy();
     spaceship.addImage(spaceshipimg2);
-    moveStars(180, 1);
+    moveStars(180);
     if(spaceship.y < displayHeight-displayHeight/3){
       gamestate = "countdown";
       playSound(mySound);
@@ -65,7 +71,7 @@ function draw() {
 
   else if(gamestate === "countdown"){
     countdown.visible = true;
-    moveStars(180, 1);
+    moveStars(180);
     if(countdwn >= 3){
       countdown.addImage(countdownimg3);
     }
@@ -86,14 +92,18 @@ function draw() {
     if(countdwn === -1){
       gamestate = "final";
       countdown.destroy();
+      mouseX = spaceship.x;
+      mouseY = spaceship.y;
     }
   }
 
   else if(gamestate === "final"){
+    incspd = 2;
     spaceship.addImage(spaceshipimg3);
-    moveStars(180, 2);
+    moveStars(180);
     countScore();
     movePlayer();
+    pauseAndPlay();
   }
 }
 
@@ -105,21 +115,47 @@ function createStars(number){
   for(var i = 0; i < number; i++){
     var x = random(0, displayWidth);
     var y = random(-20, displayHeight);
-    var star = createSprite(x, y, 1, 1);
+    var star = createSprite(x, y, 10, 10);
     star.addImage(starimg);
-    star.scale = random(0.01, 0.1);
+    star.scale = random(0.01, 0.125);
     star.depth = spaceship.depth - 1;
+    // var tintcolor = Math.round(random(1,3));
+    //   switch(tintcolor){
+    //     case 1:
+    //       fill("rgba(0,0,255,255)");
+    //       console.log("blue")  
+    //       break;
+    //     case 2:
+    //       fill("rgba(200,0,0,255)");
+    //       console.log("red")
+    //       break;
+    //     case 3:
+    //       var red = Math.round(random(200,255));
+    //       var green = Math.round(random(200,255));
+    //       tint(red,green,0,255);
+    //       break;
+    //     default:
+    //       star.tint = "rgba(255,255,255,255)";
+    //       star.addImage(starimg);
+    //   }
     stararr.push(star);
   }
 }
 
-function moveStars(number, velocity){
-  for(var i = 0; i < number; i++){
-    stararr[i].velocityY = velocity;
-    if(stararr[i].y > displayWidth/2){
-      stararr[i].y = random(-40,-10);
-      stararr[i].x = random(0, displayWidth);
-      stararr[i].scale = random(0.01, 0.1);
+function moveStars(number){
+  if(pauseState === false){
+    for(var i = 0; i < number; i++){
+      stararr[i].velocityY = 1*incspd;
+      if(stararr[i].y > displayWidth/2){
+        stararr[i].y = random(-40,-10);
+        stararr[i].x = random(0, displayWidth);
+        stararr[i].scale = random(0.01, 0.1);
+      }
+    }
+  }
+  else{
+    for(var i = 0; i < number; i++){
+      stararr[i].velocityY = 0;
     }
   }
 }
@@ -129,17 +165,56 @@ function countScore(){
   textSize(20);
   textFont('Hanalei Fill');
   text("Score: " + Math.round(score), displayWidth - 120, 100);
-  if(frameCount % 7.5 === 0){
-    score += 1;
+  if(pauseState === false){
+    if(frameCount % 30 === 0){
+      score ++;
+    }
+  }
+  else{
+    score += 0;
   }
 }
 
 function movePlayer(){
-  spaceship.x = mouseX;
-  if(spaceship.x > displayWidth - 50){
-    spaceship.x = displayWidth - 50;
+  if(mouseY > displayHeight/2){
+    spaceship.x = mouseX;
+    if(spaceship.x > displayWidth - 50){
+      spaceship.x = displayWidth - 50;
+    }
+    if(spaceship.x < 50){
+      spaceship.x = 50;
+    }
   }
-  if(spaceship.x < 50){
-    spaceship.x = 50;
+}
+
+function pauseAndPlay(){
+  if(gamestate === "final"){
+    pausebutton.visible = true;
   }
+  else{
+    pausebutton.visible = false;
+  }
+  if(pauseState === false){
+    pausebutton.addImage(pauseimg);
+  }
+  else{
+    pausebutton.addImage(playimg);
+  }
+  if(frameCount % 60 === 0 && canClickPause === false){
+    canClickPause = true;
+  }
+  if(mousePressedOver(pausebutton) && canClickPause === true){
+    if(pauseState === true){
+      pauseState = false;
+      canClickPause = false;
+    }
+    else if(pauseState === false){
+      pauseState = true;
+      canClickPause = false;
+    }
+  }
+}
+
+function shootFromShip(){
+
 }
